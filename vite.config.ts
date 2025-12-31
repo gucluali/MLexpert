c6b373132ae1506fa91b7354cc34ac2633fed7f6
gucluali <alitiptem@gmail.com>
2025-12-31 12:22:52 +0300

    link update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mlexpert/', // GitHub Pages deployment path
+  base: '/MLexpert/', // GitHub Pages deployment path (Case sensitive!)
 })
